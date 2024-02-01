f01717c9b0825ef0e8868f51aa7dec385d7a6a1a
MrNyvle <elvynwakeford@gmail.com>
2024-02-01 03:22:12 +0100

    Face, need to link to sliders/scores, and also 1fps

---
+++ Assets/NewUnlitShader.shader
@@ -0,0 +1,81 @@
+Shader "Unlit/NewUnlitShader"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _Point0 ("Point0", Vector) = (0,0,0,0)
+        _Point1 ("Point1", Vector) = (0,0,0,0)
+        _Point2 ("Point2", Vector) = (0,0,0,0)
+        _Point3 ("Point3", Vector) = (0,0,0,0)
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                UNITY_FOG_COORDS(1)
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_TexelSize;
+            float4 _MainTex_ST;
+            vector _Point0;
+            vector _Point1;
+            vector _Point2;
+            vector _Point3;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                UNITY_TRANSFER_FOG(o,o.vertex);
+                return o;
+            }
+
+            float2 beszier (float t)
+            {
+                return  (1-t)*(1-t)*(1-t) * _Point0 + 3 * (1-t)*(1-t) * t * _Point1 + 3 * (1-t) * t*t * _Point2 + t*t*t * _Point3;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // sample the texture
+                fixed4 col = tex2D(_MainTex, i.uv);
+
+                float2 currentUV = _MainTex_TexelSize * i.uv;
+                int bezierValue = beszier(currentUV.x);
+                if (bezierValue == currentUV.y)
+                {
+                    col = vector (0,0,0,1);
+                }
+                
+                // apply fog
+                UNITY_APPLY_FOG(i.fogCoord, col);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
--- Assets/_Scripts/Face/MouthFiller.cs
+++ Assets/_Scripts/Face/MouthFiller.cs
@@ -1,62 +1,124 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NaughtyAttributes;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 [RequireComponent(typeof(SpriteRenderer))]
 public class MouthFiller : MonoBehaviour
 {
-    [SerializeField] private List<BezierCurves> _bezierCurvesList;
-    
-    private SpriteRenderer _spriteRenderer;
-    public int size;
-    public int sizeTexture;
-    public int offset;
-    
-    
-    
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private SpriteMask _spriteMask;
+    [SerializeField] private int sizeTexture;
+    [SerializeField] private List<Transform> Beziers = new List<Transform>();
+
+    [SerializeField] private Color mouthColor;
+    [SerializeField] private Color otherColor;
+
+
+    private void Update()
     {
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-        
-        Texture2D  texture2D = new Texture2D(sizeTexture, sizeTexture);
-
-        Sprite sprite = Sprite.Create(texture2D, new Rect(0,0, sizeTexture,sizeTexture), new Vector2(0.5f,0.5f), sizeTexture);
-
-        _spriteRenderer.sprite = sprite;
-
+	    //if uncoment this prepare for death
+	    //UpdateGraph();
     }
-    
-    
+
+    [Button("UpdateMesh")]
     private void UpdateGraph()
     {
-        //DrawBezier(_spriteRenderer, _bezierCurvesList[0]);
-        DrawBezier(_bezierCurvesList[0]);
+        Texture2D texture2D = new Texture2D(sizeTexture, sizeTexture);
+
+        Sprite sprite = Sprite.Create(texture2D, new Rect(0,0, sizeTexture,sizeTexture), new Vector2(0.5f,0.5f), sizeTexture/2, 0, SpriteMeshType.FullRect);
+
+        _spriteRenderer.sprite = sprite;
+        
+        ResetImage();
+        DrawBezier(Beziers.Take(4).ToList());
+        DrawBezier(Beziers.Skip(4).Take(4).ToList());
+        
+        Vector2 origin = FloodFillOrigin();
+        
+        if (origin != Vector2.zero)
+        {
+
+	        Texture2D writetexture2D = new Texture2D(texture2D.width, texture2D.height);
+	        writetexture2D.SetPixels(texture2D.GetPixels());
+	        writetexture2D.Apply();
+	        FloodFill(_spriteRenderer.sprite.texture, writetexture2D, otherColor, 0, (int)origin.x, (int)origin.y);
+	        Sprite spriteNew = Sprite.Create(writetexture2D, new Rect(0,0, sizeTexture,sizeTexture), new Vector2(0.5f,0.5f), sizeTexture/2,0, SpriteMeshType.FullRect);
+	        _spriteMask.sprite = spriteNew;
+        }
     }
 
-    private void DrawBezier(BezierCurves bezierCurves)
+    private Vector2 FloodFillOrigin()
     {
-        
-        for (int i = 0; i < _spriteRenderer.sprite.texture.width; i++)
+	    int blackCounter = 0;
+	    bool isNextWhite = false;
+	    Vector2 point = Vector2.zero;
+	    
+	    for (int i = 0; i < sizeTexture; i++)
+	    {
+		    Color color = _spriteRenderer.sprite.texture.GetPixel(sizeTexture / 2,i );
+
+		    if (color == mouthColor)
+		    {
+			    blackCounter++;
+			    Color colorWhite = _spriteRenderer.sprite.texture.GetPixel(sizeTexture / 2,i+1);
+			    if (colorWhite == otherColor && point == Vector2.zero)
+			    {
+				    isNextWhite = true;
+                               
+				    point = new Vector2(sizeTexture / 2, i + 1);
+			    }
+		    }
+	    }
+
+	    if (blackCounter >= 2 && isNextWhite)
+	    {
+		    //Debug.Log($"True : {point.ToString()}");
+		    return point;
+	    }
+
+	    //Debug.Log("False");
+	    return point;
+    }
+
+    private void ResetImage()
+    {
+	    //TODO : this is slow af
+	    
+	    int width = _spriteRenderer.sprite.texture.width;
+	    int height = _spriteRenderer.sprite.texture.height;
+	    
+        for (int i = 0; i < width; i++)
         {
-            for (int j = 0; j < _spriteRenderer.sprite.texture.height; j++)
+            for (int j = 0; j < height; j++)
             {
-                _spriteRenderer.sprite.texture.SetPixel(i, j, Color.white);
+                _spriteRenderer.sprite.texture.SetPixel(i, j, otherColor);
             }
         }
-        
-        List<Vector2> Points = CubeBezier(
-            bezierCurves.Points[0].position,
-            bezierCurves.Points[1].position,
-            bezierCurves.Points[2].position,
-            bezierCurves.Points[3].position,
+    }
+
+    private void DrawBezier(List<Transform> Points)
+    {
+        List<Vector2> BesierPoints = CubeBezier(
+            Points[0].position,
+            Points[1].position,
+            Points[2].position,
+            Points[3].position,
             _spriteRenderer.sprite.texture.width
             );
         
-        foreach (var coord in Points)
+        foreach (var coord in BesierPoints)
         {
-            _spriteRenderer.sprite.texture.SetPixel((int)(coord.x * size) + offset, (int)(coord.y * size) + offset, Color.black);
+            _spriteRenderer.sprite.texture.SetPixel((int)(coord.x * sizeTexture/2) + sizeTexture/2 , (int)(coord.y * sizeTexture/2) + sizeTexture/2, mouthColor);
+            _spriteRenderer.sprite.texture.SetPixel((int)(coord.x * sizeTexture/2) + sizeTexture/2  - 1, (int)(coord.y * sizeTexture/2) + sizeTexture/2, mouthColor);
+            _spriteRenderer.sprite.texture.SetPixel((int)(coord.x * sizeTexture/2) + sizeTexture/2 , (int)(coord.y * sizeTexture/2) + sizeTexture/2 - 1, mouthColor);
+            _spriteRenderer.sprite.texture.SetPixel((int)(coord.x * sizeTexture/2) + sizeTexture/2  + 1, (int)(coord.y * sizeTexture/2) + sizeTexture/2, mouthColor);
+            _spriteRenderer.sprite.texture.SetPixel((int)(coord.x * sizeTexture/2) + sizeTexture/2  , (int)(coord.y * sizeTexture/2) + sizeTexture/2 + 1, mouthColor);
         }
         _spriteRenderer.sprite.texture.Apply();
         
@@ -78,4 +140,76 @@ public class MouthFiller : MonoBehaviour
 
         return points;
     }
+    
+    
+	public struct Point {
+
+		public int x;
+		public int y;
+
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+
+	public void FloodFill(Texture2D readTexture, Texture2D writeTexture, Color sourceColor, float tollerance, int x, int y) {
+		var targetColor = mouthColor;
+		var q = new Queue<Point> (readTexture.width * readTexture.height);
+		q.Enqueue (new Point (x, y));
+		int iterations = 0;
+
+		var width = readTexture.width;
+		var height = readTexture.height;
+		while (q.Count > 0) {
+			var point = q.Dequeue ();
+			var x1 = point.x;
+			var y1 = point.y;
+			if (q.Count > width * height) {
+				throw new System.Exception ("The algorithm is probably looping. Queue size: " + q.Count);
+			}
+
+			if (writeTexture.GetPixel (x1, y1) == targetColor) {
+				continue;
+			}
+
+			writeTexture.SetPixel (x1, y1, targetColor);
+
+
+			var newPoint = new Point (x1 + 1, y1);
+			if (CheckValidity (readTexture, readTexture.width, readTexture.height, newPoint, sourceColor, tollerance))
+				q.Enqueue (newPoint);
+
+			newPoint = new Point (x1 - 1, y1);
+			if (CheckValidity (readTexture, readTexture.width, readTexture.height, newPoint, sourceColor, tollerance))
+				q.Enqueue (newPoint);
+
+			newPoint = new Point (x1, y1 + 1);
+			if (CheckValidity (readTexture, readTexture.width, readTexture.height, newPoint, sourceColor, tollerance))
+				q.Enqueue (newPoint);
+
+			newPoint = new Point (x1, y1 - 1);
+			if (CheckValidity (readTexture, readTexture.width, readTexture.height, newPoint, sourceColor, tollerance))
+				q.Enqueue (newPoint);
+
+			iterations++;
+		}
+		
+		writeTexture.Apply();
+	}
+
+	static bool CheckValidity(Texture2D texture, int width, int height, Point p, Color sourceColor, float tollerance) {
+		if (p.x < 0 || p.x >= width) {
+			return false;
+		}
+		if (p.y < 0 || p.y >= height) {
+			return false;
+		}
+
+		var color = texture.GetPixel(p.x, p.y);
+
+		var distance = Mathf.Abs (color.r - sourceColor.r) +  Mathf.Abs (color.g - sourceColor.g) +  Mathf.Abs (color.b - sourceColor.b);
+		return distance <= tollerance;
+	}
+    
 }
