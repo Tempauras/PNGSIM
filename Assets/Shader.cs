8bda2c7d75ec99d3c22abf0fda44165169ae327a
MrNyvle <elvynwakeford@gmail.com>
2024-02-01 20:05:00 +0100

    compute shaderzzzzzz

--- Assets/Shader.cs
+++ Assets/Shader.cs
@@ -5,7 +5,7 @@ using NaughtyAttributes;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(SpriteMask))]
 public class Shader : MonoBehaviour
 {
     [SerializeField] private Camera mainCam;
@@ -13,14 +13,19 @@ public class Shader : MonoBehaviour
     [SerializeField] private ComputeShader computeShader;
     [SerializeField] private RenderTexture renderTexture;
     [SerializeField] private int texSize = 256;
+    [SerializeField] private Vector2 offset;
+    [SerializeField] private float scale;
+    [SerializeField] private float tolerance;
     
-    private SpriteRenderer _spriteRenderer;
+    
+    
+    private SpriteMask _spriteRenderer;
     public Texture2D texture2D;
     public Sprite sprite;
 
     private void Start()
     {
-        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer = GetComponent<SpriteMask>();
 
         renderTexture = new RenderTexture(texSize, texSize, texSize);
         renderTexture.enableRandomWrite = true;
@@ -32,6 +37,7 @@ public class Shader : MonoBehaviour
         computeShader.Dispatch(0, renderTexture.width/8, renderTexture.height/8, 1);
         
         texture2D = new Texture2D(renderTexture.width, renderTexture.height);
+        texture2D.filterMode = FilterMode.Point;
         
         sprite = Sprite.Create(texture2D, new Rect(0,0, texSize,texSize), new Vector2(0.5f,0.5f), 100);
 
@@ -42,14 +48,41 @@ public class Shader : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        computeShader.SetVector("point0" , new Vector4(points[0].position.x,points[0].position.y,points[0].position.z, 0));
-        computeShader.SetVector("point1" , new Vector4(points[1].position.x,points[1].position.y,points[1].position.z, 0));
-        computeShader.SetVector("point2" , new Vector4(points[2].position.x,points[2].position.y,points[2].position.z, 0));
-        computeShader.SetVector("point3" , new Vector4(points[3].position.x,points[3].position.y,points[3].position.z, 0));
+        // computeShader.SetVector("point0" , new Vector4(points[0].position.x*scale+offset.x,points[0].position.y*scale+offset.y,points[0].position.z, 0));
+        // computeShader.SetVector("point1" , new Vector4(points[1].position.x*scale+offset.x,points[1].position.y*scale+offset.y,points[1].position.z, 0));
+        // computeShader.SetVector("point2" , new Vector4(points[2].position.x*scale+offset.x,points[2].position.y*scale+offset.y,points[2].position.z, 0));
+        // computeShader.SetVector("point3" , new Vector4(points[3].position.x*scale+offset.x,points[3].position.y*scale+offset.y,points[3].position.z, 0));
+        
+        computeShader.SetFloat("thickness", tolerance);
+
+
+        List<Vector2> bezierPoints = points.ConvertAll(x =>
+        {
+            Vector3 position;
+            return new Vector2((position = x.position).x*scale+offset.x, position.y*scale+offset.y);
+        });
+
+        int sizeOfVect2 = sizeof(float) * 2;
+
+        ComputeBuffer computeBuffer = new ComputeBuffer(bezierPoints.Count, sizeOfVect2);
+        computeBuffer.SetData(bezierPoints);
+        
+        computeShader.SetBuffer(0, "pointsBuffer", computeBuffer);
         
         computeShader.Dispatch(0, renderTexture.width/8, renderTexture.height/8, 1);
+
+        Vector2[] temp = new Vector2[bezierPoints.Count];
+        
+        // computeBuffer.GetData(temp);
+        //
+        // foreach (var VARIABLE in temp)
+        // {
+        //     Debug.Log(VARIABLE.ToString());
+        // }
         
         UpdateTextureFromRenderTexture(renderTexture);
+        
+        computeBuffer.Dispose();
         
     }
     
