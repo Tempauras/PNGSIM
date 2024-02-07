4f9f3b1ee2f90683f63a767613834b6bd1bfd8d5
MrNyvle <elvynwakeford@gmail.com>
2024-02-07 15:56:20 +0100

    smile in compute shader

--- Assets/Shader.cs
+++ Assets/Shader.cs
@@ -1,103 +1,117 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using NaughtyAttributes;
+using System.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.Serialization;
 
-[RequireComponent(typeof(SpriteMask))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class Shader : MonoBehaviour
 {
     [SerializeField] private Camera mainCam;
     [SerializeField] private List<Transform> points;
     [SerializeField] private ComputeShader computeShader;
     [SerializeField] private RenderTexture renderTexture;
-    [SerializeField] private int texSize = 256;
     [SerializeField] private Vector2 offset;
     [SerializeField] private float scale;
-    [SerializeField] private float tolerance;
+    [SerializeField] private float thickness;
     
-    
-    
-    private SpriteMask _spriteRenderer;
-    public Texture2D texture2D;
-    public Sprite sprite;
+    private SpriteRenderer _spriteRenderer;
+
+    private int _kernelDraw;
+    private int _kernelCalculateBezier;
+    private ComputeBuffer _pointBuffer;
+    private ComputeBuffer _computeBuffer;
 
     private void Start()
     {
-        _spriteRenderer = GetComponent<SpriteMask>();
-
-        renderTexture = new RenderTexture(texSize, texSize, texSize);
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        
+        //create a render texture for the compute shader to draw on 
+        renderTexture = new RenderTexture(mainCam.pixelWidth, mainCam.pixelHeight, 0);
         renderTexture.enableRandomWrite = true;
         renderTexture.Create();
-
-        computeShader.SetTexture(0,"Result" ,renderTexture);
-        computeShader.SetFloat("texSize" ,texSize);
         
-        computeShader.Dispatch(0, renderTexture.width/8, renderTexture.height/8, 1);
-        
-        texture2D = new Texture2D(renderTexture.width, renderTexture.height);
+        //Setup sprite for sprite renderer
+        Texture2D texture2D = new Texture2D(renderTexture.width, renderTexture.height);
         texture2D.filterMode = FilterMode.Point;
         
-        sprite = Sprite.Create(texture2D, new Rect(0,0, texSize,texSize), new Vector2(0.5f,0.5f), 100);
-
+        Sprite sprite = Sprite.Create(texture2D, new Rect(0,0, texture2D.width,texture2D.height), new Vector2(0.5f,0.5f), 100);
         _spriteRenderer.sprite = sprite;
+        
+        //Add texture to shader on material
+        _spriteRenderer.material.SetTexture("_RenderTex", renderTexture);
+
+        //Setting up the compute shader
+        
+        //getting kernels
+        _kernelDraw = computeShader.FindKernel("CSDraw");
+        _kernelCalculateBezier = computeShader.FindKernel("CalculateBezierPoints");
+        
+        //setting variables to draw in compute shader
+        computeShader.SetTexture(_kernelDraw,"Result" , renderTexture);
+        computeShader.SetInt("size", renderTexture.width);
+        computeShader.SetFloat("thickness", thickness);
+        
+        //setup point buffer to use in compute shader, essentially making a semi-dynamic array
+        int sizeOfVect2 = sizeof(float) * 2;
+        _computeBuffer = new ComputeBuffer(renderTexture.width*2, sizeOfVect2);
+        _pointBuffer = new ComputeBuffer(points.Count, sizeOfVect2);
+        UpdatePointBuffer();
+        Vector2[] arrayForBezierPoints = new Vector2[renderTexture.width*2];
+        _computeBuffer.SetData(arrayForBezierPoints);
+        
+        //giving that array to both kernels of compute shader
+        computeShader.SetBuffer(_kernelDraw, "points_buffer",  _computeBuffer);
+        computeShader.SetBuffer(_kernelCalculateBezier, "points_buffer",  _computeBuffer);
+
+        UpdateFrame();
 
     }
 
-    // Update is called once per frame
-    void Update()
+    private async Task UpdateFrame()
     {
-        // computeShader.SetVector("point0" , new Vector4(points[0].position.x*scale+offset.x,points[0].position.y*scale+offset.y,points[0].position.z, 0));
-        // computeShader.SetVector("point1" , new Vector4(points[1].position.x*scale+offset.x,points[1].position.y*scale+offset.y,points[1].position.z, 0));
-        // computeShader.SetVector("point2" , new Vector4(points[2].position.x*scale+offset.x,points[2].position.y*scale+offset.y,points[2].position.z, 0));
-        // computeShader.SetVector("point3" , new Vector4(points[3].position.x*scale+offset.x,points[3].position.y*scale+offset.y,points[3].position.z, 0));
+        //updates the 4 points of the 2 bezier we want to draw 
         
-        computeShader.SetFloat("thickness", tolerance);
+        UpdatePointBuffer();
+        computeShader.SetBuffer(_kernelCalculateBezier, "points",  _pointBuffer);
+        
+        //calls the dispatch, essentially tells the compute shaders to run 
+        
+        computeShader.Dispatch(_kernelCalculateBezier, 1,1,1);
+        
+        computeShader.Dispatch(_kernelDraw, renderTexture.width, renderTexture.height, 1);
 
+        await Task.Delay(15);
 
+        UpdateFrame();
+
+    }
+
+    
+
+    private void UpdatePointBuffer()
+    {
+        // creates a list of screen space vector2 from the transform points of the bezier  
         List<Vector2> bezierPoints = points.ConvertAll(x =>
         {
             Vector3 position;
-            return new Vector2((position = x.position).x*scale+offset.x, position.y*scale+offset.y);
+            Vector2 pos = new Vector2((position = x.position).x, position.y);
+            return (Vector2)mainCam.WorldToScreenPoint(new Vector2(pos.x*scale+offset.x, pos.y*scale+offset.y));
         });
-
-        int sizeOfVect2 = sizeof(float) * 2;
-
-        ComputeBuffer computeBuffer = new ComputeBuffer(bezierPoints.Count, sizeOfVect2);
-        computeBuffer.SetData(bezierPoints);
         
-        computeShader.SetBuffer(0, "pointsBuffer", computeBuffer);
-        
-        computeShader.Dispatch(0, renderTexture.width/8, renderTexture.height/8, 1);
-
-        Vector2[] temp = new Vector2[bezierPoints.Count];
-        
-        // computeBuffer.GetData(temp);
-        //
-        // foreach (var VARIABLE in temp)
-        // {
-        //     Debug.Log(VARIABLE.ToString());
-        // }
-        
-        UpdateTextureFromRenderTexture(renderTexture);
-        
-        computeBuffer.Dispose();
-        
+        //sets the data to the compute buffer
+        _pointBuffer.SetData(bezierPoints);
     }
-    
-    public void UpdateTextureFromRenderTexture(RenderTexture rTex)
+
+    private void OnDisable()
     {
-        RenderTexture currentActiveRT = RenderTexture.active;
-        RenderTexture.active = rTex;
+        //release everything, no longer needed
+        _computeBuffer.Release();
+        _pointBuffer.Release();
+    }
 
-        mainCam.Render();
-
-        // Create a new Texture2D and read the RenderTexture image into it
-        
-        texture2D.ReadPixels(new Rect(0, 0, texture2D.width, texture2D.height), 0, 0);
-        texture2D.Apply();
-
-        RenderTexture.active = currentActiveRT;
+    private void OnDestroy()
+    {
+        //release everything, no longer needed
+        _computeBuffer.Release();
+        _pointBuffer.Release();
     }
 }
